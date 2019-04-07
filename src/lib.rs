d93afa4a9b8980f4f5150bd619edeeb34f2ca79e
M@ Dunlap <mrd@vicarious.com>
2019-04-07 15:45:31 -0700

    added the basics

---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+//! UOM Supplemental is a companion library for UOM that adds `Moment`s, and whatever
+//! other features harmonize nicely with that library but aren't allowed in.
+//!
+
+#![deny(
+    dead_code,
+    missing_docs,
+    unsafe_code,
+    unused_imports,
+    unused_parens,
+    unused_qualifications
+)]
+
+extern crate uom;
